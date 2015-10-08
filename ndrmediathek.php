df514ebdaa6650ec049e060190107b1f732d468d
Daniel Gehn <inad.gamer@gmail.com>
2015-10-08 21:29:35 +0200

    Fixed Hosting Provider by adjusting regexes

--- ndrmediathek.php
+++ ndrmediathek.php
@@ -2,7 +2,7 @@
 
 /**
  * @author Daniel Gehn <me@theinad.com>
- * @version 0.3b
+ * @version 0.3c
  * @copyright 2015 Daniel Gehn
  * @license http://opensource.org/licenses/MIT Licensed under MIT License
  */
@@ -22,18 +22,15 @@ class SynoFileHostingNDRMediathek extends TheiNaDProvider {
             return false;
         }
 
-        preg_match('#playlist:\s*\[\s*\{(.*?)\}\s*]#is', $rawXML, $matches);
-
-        preg_match_all('#\d:\s*\{\s*src\s*:\s*["|\'](.*?)["|\']#si', $matches[1], $sources);
+        preg_match('#itemprop="contentUrl" content="(.*?)"#is', $rawXML, $sources);
 
         $url = '';
 
-        foreach($sources[1] as $source)
+        $source = $sources[1];
+
+        if(strpos($source, "media.ndr.de") !== false)
         {
-            if(strpos($source, "media.ndr.de") !== false)
-            {
-                $url = $source;
-            }
+            $url = $source;
         }
 
         $this->DebugLog('Best format is ' . $url);
@@ -46,16 +43,23 @@ class SynoFileHostingNDRMediathek extends TheiNaDProvider {
 
         $match = array();
 
-        if(preg_match('#var trackTitle = "(.*?)";#i', $rawXML, $match) == 1)
+        if(preg_match('#itemprop="name" content="(.*?)"#is', $rawXML, $match) == 1)
         {
-            $episodeTitle = $match[1];
+            $episodeTitle = str_replace("Video: ", "", $match[1]);
             $filename .= $episodeTitle;
         }
-        else
-        {
-            $filename .= $pathinfo['basename'];
-        }
 
+        if($filename == "" && preg_match('#itemprop="headline">(.*?)<\/#is', $rawXML, $headline) == 1)
+        {
+            $episodeTitle = $headline[1];
+
+            if(preg_match('#itemprop="startDate" content="(.*?)">#is', $rawXML, $startDate) == 1)
+            {
+                $episodeTitle .= " vom " . date("d.m.Y", strtotime($startDate[1]));
+            }
+
+            $filename .= $episodeTitle;
+        }
 
         if(empty($filename))
         {
